1c16d16169957fb92c8a36e1db0b5f9bb89d9e99
Chris <chris@CMainPC>
2023-04-01 19:48:27 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+
+int numberOfClients = 100;
+string httpEndpoint = "https://placeholder/DeviceDataEndpoint";
+string authToken = "placeholder";
+
+if (args.Length == 3)
+{
+    int.TryParse(args[0], out numberOfClients);
+    httpEndpoint = args[1];
+    authToken = args[2];
+}
+
+List<BogusDataClient> bogusDataClients = new();
+
+for (int i = 0; i < numberOfClients; i++)
+{
+    bogusDataClients.Add(new BogusDataClient(httpEndpoint, authToken));
+}
+
+foreach (BogusDataClient bogusDataClient in bogusDataClients) 
+{
+    Console.WriteLine($"Initialising bogus data client: {bogusDataClient.ComputerName}");
+    bogusDataClient.Start();
+}
+
+for (int i = 0; i < 30; i++)
+{
+    Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@ Finished initialising clients @@@@@@@@@@@@@@@@@@@@@@@@");
+}
+
+while (true) 
+{ 
+    Task.Delay(1000);
+}
+
+public class BogusDataClient
+{
+    private readonly string _computerNamePrefix = "FakeHost-";
+    private readonly int _dataCollectionInterval = 60000;
+    public string Endpoint { get; }
+    public string DeviceId { get; } = Guid.NewGuid().ToString();
+    public string AzureADDeviceId { get; } = Guid.NewGuid().ToString();
+    public string AuthorizationToken { get; }
+    public string ComputerName { get; }
+    public DateTime LastBootUpTime { get; set; }
+    public int TotalPhysicalMemoryMB { get; set; }
+
+
+    public BogusDataClient(string endpoint, string authToken)
+    {
+        Endpoint = endpoint;
+        AuthorizationToken = authToken;
+
+        ComputerName = _computerNamePrefix + Guid.NewGuid().ToString().Replace("-","").Substring(26);
+
+        Random random = new Random();
+
+        DateTime currentDate = DateTime.UtcNow;
+        int daysAgo = random.Next(2, 100);
+
+        TimeSpan timeAgo = TimeSpan.FromDays(daysAgo);
+        long ticksAgo = timeAgo.Ticks;
+
+        long randomTicks = (long)(random.NextDouble() * ticksAgo);
+        TimeSpan randomTimeSpan = new TimeSpan(randomTicks);
+        LastBootUpTime = currentDate.Subtract(timeAgo).Add(randomTimeSpan);
+
+        TotalPhysicalMemoryMB = (int)Math.Round(random.Next(1000,256000) / 1000d, 0) * 1000;
+    }
+
+    public async void Start()
+    {
+        Random random = new();
+        int randomStartDelay = random.Next(1, 120000);
+        await Task.Delay(randomStartDelay);
+
+        Console.WriteLine("-------  SENDING FIRST REQUEST -------");
+        while (true)
+        {
+            var data = new 
+            {
+                deviceId = DeviceId,
+                azureAdDeviceId = AzureADDeviceId,
+                ComputerName = ComputerName,
+                LastBootUpTime = LastBootUpTime,
+                UptimeTotalDays = (DateTime.Now - LastBootUpTime).TotalDays,
+                FreePhysicalMemoryMB = random.Next((int)Math.Round((long)TotalPhysicalMemoryMB*0.85), (int)Math.Round((long)TotalPhysicalMemoryMB*0.98)),
+                TotalPhysicalMemoryMB = TotalPhysicalMemoryMB,
+                CpuLoad = random.Next(0,10),
+                ProcessCount = random.Next(100,140),
+                FreeStorage = random.Next(1000,1200),
+                PingMs = random.Next(1,3),
+                OSEnvironment = RuntimeInformation.OSDescription,
+            };
+
+            string jsonData = JsonSerializer.Serialize(data);
+            Console.WriteLine($"Sending data {jsonData}");
+
+            using (HttpClient httpClient = new())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", AuthorizationToken);
+                httpClient.DefaultRequestHeaders.Add("DeviceId", DeviceId);
+                
+                _ = await httpClient.PostAsync(Endpoint, new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"));
+            }
+
+            Console.WriteLine("Response received");
+
+            await Task.Delay(_dataCollectionInterval);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Device Data Ingestion Stress Test
+
+This is a simple stress test for the device data ingestion service. It is intended to be run on a single machine, and will generate a large number of devices and send data to the ingestion service.
+
+Related:
+ - [Device Data Ingestion Service]()
+ - [Device Data Ingestion Client]()
+
+------------------
+## To build
+Requirements:
+1. .NET 7.0 SDK. Other versions may work.
+2. Windows or Linux
+3. Change the placeholder API endpoint and authentication token in code
+4. Run the below commands in the project directory.
+5. You can now run the executables
+
+The below commands will build the project and output single, portable executable files.
+
+### Build for Windows
+```dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true```
+
+### Build for Linux
+```dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true```
